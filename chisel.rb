73183a30793f1ee2236dde1133e4c2dfc9c15b9e
Chelsea Worrel <chelseaworrel@Chelseas-MacBook-Pro.local>
2015-02-19 14:27:14 -0700

    begin: tests

--- chisel.rb
+++ chisel.rb
@@ -2,6 +2,24 @@
 #parser: analyze (a string or text) into logical syntactic components.
 
 class Chisel
-  header = '# My Life in Desserts'
-  puts header.gsub(header, "<h1>My Life in Desserts</h1>")
+
+  def parse(text)
+    text.gsub(text, '<h1>My Life in Desserts</h1>')
+    #use an enumerable to 'find' the key: # and then call the gsub method on that
+    #do this for each thing we are calling
+  end
+
+  # def line_split(string)
+  #   puts string.split("/n").count
+  #
+  # end
 end
+
+#use sub! to change symbols like <h1> </h1> and change them individually
+#so basically: your text is going to remain the same, you need to make sure it creates
+#chunks of text, paragraphs so that everything is arranged in the same format
+#you are going to go through (iterate) over the 'document' and change all of the
+#symbols, example: #, ##, <h1>, * , probably use a map, select, or find enumerable
+#with an if * sub <em> : rememember that it is going to go over the whole document (iterate)
+#over the document and it will change the first instance of * to <em> while changing the next
+#instance of * with </em> using sub! 
--- chisel_test.rb
+++ chisel_test.rb
@@ -8,8 +8,74 @@ class ChiselTest < Minitest::Test
     assert Chisel
   end
 
-  def turn_header_into_html
-    header = Chisel.new
-    assert_equal "'<h1>My Life in Desserts</h1>'", header.gsub
+  def test_that_it_parses_the_subheader
+   skip
+    parser = Chisel.new
+    assert_equal #[2, 4, 6, 8, 10], evens
+
   end
+
+  def test_that_it_parses_the_header
+  skip
+    parser = Chisel.new
+    assert_equal '<h1>My Life in Desserts</h1>', parsed.parse('# My Life in Desserts')
+  end
+
+  def test_that_it_has_a_chunk_of_text
+  skip
+    parser = Chisel.new
+    assert_equal #[2, 4, 6, 8, 10], evens
+
+  end
+
+  def test_that_it_has_2_chunks_of_text
+  skip
+    parser = Chisel.new
+    assert_equal #[2, 4, 6, 8, 10], evens
+
+  end
+
+  def test_that_it_has_a_paragraph
+  skip
+    parser = Chisel.new
+    assert_equal #[2, 4, 6, 8, 10], evens
+  end
+
+
+  def test_it_has_an_astric
+  skip
+  parser = Chisel.new
+  assert_equal #[2, 4, 6, 8, 10], evens
+  end
+
+  def test_it_has_a_double_astric
+  skip
+  parser = Chisel.new
+  assert_equal #[2, 4, 6, 8, 10], evens
+  end
+
+  def test_it_is_bold
+  skip
+  parser = Chisel.new
+  assert_equal #[2, 4, 6, 8, 10], evens
+  end
+
+  def test_it_ends_the_paragraph
+  skip
+  parser = Chisel.new
+  assert_equal #[2, 4, 6, 8, 10], evens
+  end
+
+#at the end put the output
+
+
+#   def test_paragraphs
+#     paragraphs = Chisel.new
+#     string = '# My Life in Desserts
+# ## Chapter 1: The Beginning
+# "You just *have* to try the cheesecake," he said. "Ever since it appeared in
+# **Food & Wine** this place has been packed every night."'
+#     assert_equal 4, paragraphs.line_split(string).count
+#   end
+
 end
